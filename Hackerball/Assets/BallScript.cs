8d4b45d8679c991ba20eafe13dba480ffd56a1d3
Ted Eriksson <ted.eriksson@me.com>
2014-02-17 18:46:46 +0000

    made the project

---
+++ Hackerball/Assets/BallScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	public float moveForce = 4.0f;        
+	public float maxSpeed = 10f;
+	public float bounceHeight = 100f;
+	
+	private Vector3 v;
+	private Vector3 bounce;
+
+	private bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+		bounce = new Vector3 (0, bounceHeight, 0);
+	}
+
+	void Update () {
+		v = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+	}
+	
+	void FixedUpdate() {
+		rigidbody.velocity = Vector3.ClampMagnitude (rigidbody.velocity, maxSpeed);
+		rigidbody.AddForce(v.normalized * moveForce);
+		if (jump) {
+			jump = false;
+			rigidbody.AddForce (bounce);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "boost") {
+			jump = true;
+		}
+	}
+}
---
+++ Hackerball/Assets/Outline Unlit.shader
@@ -0,0 +1,16 @@
+﻿Shader "Custom/Outline Unlit" {
+	Properties {
+		_Color ("Main Color", Color) = (0.5,0.5,0.5,1)
+		_OutlineColor ("Outline Color", Color) = (0,0,0,1)
+		_Outline ("Outline width", Range (.002, 0.03)) = .005
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+	}
+
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		 Color [_Color]
+
+    Pass {}
+		UsePass "Toon/Basic Outline/OUTLINE"
+	} 
+}
