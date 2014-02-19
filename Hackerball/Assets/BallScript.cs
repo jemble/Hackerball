9f30835440d20e1736279c1c8d3b286073cadb94
jemble <jeremy.bourgein@gmail.com>
2014-02-19 22:53:03 +0000

    got camera working but changed ball script to relative movement

--- Hackerball/Assets/BallScript.cs
+++ Hackerball/Assets/BallScript.cs
@@ -6,6 +6,8 @@ public class BallScript : MonoBehaviour {
 	public float moveForce = 4.0f;        
 	public float maxSpeed = 10f;
 	public float bounceHeight = 100f;
+	public Vector3 startPos;
+	public float startRot;
 	
 	private Vector3 v;
 	private Vector3 bounce;
@@ -18,21 +20,37 @@ public class BallScript : MonoBehaviour {
 	}
 
 	void Update () {
-		v = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+		v = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, Input.GetAxis ("Vertical"));
+		if (transform.position.y < -10) {
+			MoveToStart();
+		}
+	
 	}
 	
 	void FixedUpdate() {
 		rigidbody.velocity = Vector3.ClampMagnitude (rigidbody.velocity, maxSpeed);
-		rigidbody.AddForce(v.normalized * moveForce);
+	//	rigidbody.AddForce(v.normalized * moveForce);
+
+	//	camera.transform.TransformDirection(v);
+		rigidbody.AddRelativeForce (v.normalized * moveForce);
+
 		if (jump) {
 			jump = false;
 			rigidbody.AddForce (bounce);
 		}
+
 	}
 
 	void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.tag == "boost") {
 			jump = true;
 		}
+	}
+
+	void MoveToStart(){
+		transform.position = startPos;
+		bounce = new Vector3 (0, bounceHeight, 0);
+		jump = false;
+
 	}
 }
---
+++ Hackerball/Assets/movaCam.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movaCam : MonoBehaviour {
+
+	public GameObject player;
+	public float zoomSpeed = 2.0f;
+	public float leftSpeed = 2.0f;
+	public float upSpeed = 2.0f;
+
+	float rotPointX;
+	float rotPointY = 0.0f;
+	float rotPointZ;
+	Vector3 rotPoint;
+	float mouseX;
+	float mouseY;
+	float mouseScr;
+	float camRotY;
+
+	// Use this for initialization
+	void Start () {
+		rotPoint = new Vector3 ();
+		rotPoint.y = rotPointY;
+		camRotY = transform.rotation.y;
+		//player.transform.Rotate (Vector3.up, camRotY);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void LateUpdate(){
+		rotPointX = player.transform.position.x;
+		rotPointZ = player.transform.position.z;
+		rotPoint.x = rotPointX;
+		rotPoint.z = rotPointZ;
+		mouseX = Input.GetAxis("Mouse X");
+		//mouseY = Input.GetAxis ("Mouse Y");
+		mouseScr = Input.GetAxis ("Mouse ScrollWheel");
+
+		camera.fieldOfView += mouseScr * zoomSpeed;
+
+		transform.LookAt (rotPoint);
+		transform.RotateAround(rotPoint,Vector3.up,mouseX * upSpeed);
+
+		float camRotY = transform.eulerAngles.y;
+		player.transform.eulerAngles = new Vector3 (0, camRotY, 0);
+
+
+		Debug.Log ("ball rot: "+ player.transform.rotation.y);
+		Debug.Log ("cam rot: "+transform.rotation.y);
+		//transform.RotateAround (rotPoint, Vector3.left, mouseY * leftSpeed);
+
+	}
+}
+
+//x and z of ball but y=0
