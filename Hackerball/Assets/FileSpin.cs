244de9afb9b5abd39343e4c0d57f5a3defa3e346
Ted Eriksson <ted.eriksson@me.com>
2014-02-26 20:59:19 +0000

    Added "Get Point" Only visuals

---
+++ Hackerball/Assets/FileSpin.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FileSpin : MonoBehaviour {
+
+	public float rotationSpeed = 75;
+	public float bounceSpeed = 5;
+	public float bounceHeight = 10;
+
+	private float currentBounce = 0;
+	private bool up = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float bounce;
+		if(up) {
+			bounce = bounceSpeed * Time.deltaTime; 
+		} else {
+			bounce = -bounceSpeed * Time.deltaTime; 
+		}
+
+		currentBounce += bounce;
+
+		if(currentBounce > bounceHeight) up = false;
+		if(currentBounce < -bounceHeight) up = true ;
+
+		this.gameObject.transform.Translate(0,bounce,0);
+
+		this.gameObject.transform.Rotate(new Vector3(0,(Time.deltaTime * rotationSpeed),0));
+	}
+}
