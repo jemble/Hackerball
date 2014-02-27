bce643a5d89ebd8fe796667cfa95867c7b3c87f3
vostrox <daveegregory@hotmail.co.uk>
2014-02-27 14:18:20 +0000

    Movement options+platormEroder to movingPlatform
    + added smashable option platform eroder

--- Hackerball/Assets/MovingBlock.cs
+++ Hackerball/Assets/MovingBlock.cs
@@ -4,8 +4,13 @@ using System.Collections;
 public class MovingBlock : MonoBehaviour {
 	Vector3 startPos;
 	 
-	public float moveHeight = 10;
-	private float posX, posZ;
+	public float xDistance = 10;
+	public float yDistance = 10;
+	public float zDistance = 10;
+	public bool xMovement = true;
+	public bool yMovement = true;
+	public bool zMovement = true;
+	private float posX, posY, posZ;
 	// Use this for initialization
 	void Start () {
 		startPos = transform.position;
@@ -13,8 +18,27 @@ public class MovingBlock : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		posX = transform.position.x;
-		posZ = transform.position.z;
-		transform.position = new Vector3 (posX, Mathf.PingPong (Time.time, moveHeight), posZ);
+		if (xMovement == true) {
+			posX = Mathf.PingPong (Time.time, xDistance);
+		}
+		else {
+			posX = transform.position.x;
+		}
+
+		if (yMovement == true) {
+			posY = Mathf.PingPong (Time.time, yDistance);
+		}
+		else {
+			posY = transform.position.y;
+		}
+
+		if (zMovement == true) {
+			posZ = Mathf.PingPong (Time.time, zDistance);
+		}
+		else {
+			posZ = transform.position.z;
+		}
+
+		transform.position = new Vector3 (posX, posY, posZ);
 	}
 }
