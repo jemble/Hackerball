ed466a2771a7316b58e1c6da80ffc98b3782d114
Ted Eriksson <ted.eriksson@me.com>
2014-03-18 22:00:41 +0000

    Added the ability to complete levels

---
+++ Hackerball/Assets/CurrentLevel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrentLevel : MonoBehaviour {
+	private int level;
+	// Use this for initialization
+	void Start () {
+		if(PlayerPrefs.HasKey("level")) {
+			level = PlayerPrefs.GetInt("level");
+		} else {
+			PlayerPrefs.SetInt("level",0);
+			level = 0;
+		}
+
+		Debug.Log(level);
+
+		GameObject[] levelBeams = GameObject.FindGameObjectsWithTag("levelBeam");
+		foreach (GameObject beam in levelBeams) {
+			goToLevel goTo = beam.GetComponent<goToLevel>();
+			if(goTo.level != level) {
+				Destroy(beam);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Hackerball/Assets/goToLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goToLevel : MonoBehaviour {
+
+	public int level;
+	public string levelName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == "Player") {
+			Application.LoadLevel(levelName);
+		}
+	}
+}
