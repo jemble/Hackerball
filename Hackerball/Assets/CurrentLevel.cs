5300df06a4b1d5cbaba0ff54ccbfb64f7b13c9e9
jemble <jeremy.bourgein@gmail.com>
2014-03-27 10:21:48 +0000

    added home scren reset level button

--- Hackerball/Assets/CurrentLevel.cs
+++ Hackerball/Assets/CurrentLevel.cs
@@ -2,18 +2,31 @@
 using System.Collections;
 
 public class CurrentLevel : MonoBehaviour {
-	private int level;
+	private static int level;
 	// Use this for initialization
 	void Start () {
+		InitLevels ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void ResetLevels(){
+		PlayerPrefs.SetInt("level",0);
+		level = 0;
+	}
+
+	public static void InitLevels(){
 		if(PlayerPrefs.HasKey("level")) {
 			level = PlayerPrefs.GetInt("level");
 		} else {
-			PlayerPrefs.SetInt("level",0);
-			level = 0;
+			ResetLevels ();
 		}
-
+		
 		Debug.Log(level);
-
+		
 		GameObject[] levelBeams = GameObject.FindGameObjectsWithTag("levelBeam");
 		foreach (GameObject beam in levelBeams) {
 			goToLevel goTo = beam.GetComponent<goToLevel>();
@@ -21,10 +34,5 @@ public class CurrentLevel : MonoBehaviour {
 				Destroy(beam);
 			}
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
---
+++ Hackerball/Assets/HomeGUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeGUI : MonoBehaviour {
+
+	public GUIStyle guiStyle = new GUIStyle();
+
+	private float screenWidth;
+	private float resetHeight;
+	private float resetWidth;
+
+	// Use this for initialization
+	void Start () {
+		screenWidth = Screen.width;
+		resetHeight = 50;
+		resetWidth = 200;
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		if(GUI.Button(new Rect((screenWidth-resetWidth),10,resetWidth,resetHeight),"reset progress",guiStyle)){
+			CurrentLevel.ResetLevels();
+			CurrentLevel.InitLevels();
+		}
+	}
+}
