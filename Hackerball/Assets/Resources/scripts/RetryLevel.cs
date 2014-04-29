02a1b1450a238d6d044f705e4c91021a7e7b037b
jemble <jeremy.bourgein@gmail.com>
2014-04-29 23:37:13 +0100

    added ability to replay levels

---
+++ Hackerball/Assets/Resources/scripts/RetryLevel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RetryLevel : MonoBehaviour {
+
+	private int bounceCount = 0;
+	public int maxBounces = 5;
+	public string levelName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == "Player") {
+			bounceCount++;
+			Debug.Log(bounceCount);
+			if(bounceCount == maxBounces){
+				Application.LoadLevel(levelName);
+			}
+		}
+	}
+}
